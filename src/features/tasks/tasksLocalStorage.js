f4acd574585af9d857c9df5d7c98e5a548557c40
Urszula Malinowska <ula.malin35@gmail.com>
2023-04-27 00:32:18 +0200

    localStorageKey created

--- src/features/tasks/tasksLocalStorage.js
+++ src/features/tasks/tasksLocalStorage.js
@@ -1,5 +1,7 @@
+const localStorageKey = "tasks";
+
 export const saveTasksInLocalStorage = (tasks) =>
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 
 export const getTasksFromLocalStorage = () =>
-  JSON.parse(localStorage.getItem("tasks")) || [];
+  JSON.parse(localStorage.getItem(localStorageKey)) || [];
