637cd9f5e7b4156c02dca2acb64327600da89d7f
Urszula Malinowska <ula.malin35@gmail.com>
2023-04-29 14:43:53 +0200

    conditional rendering of title and body in TaskPage component depending if tasked is fetched thanks to useParams

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,19 +1,26 @@
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Section from "../../../common/Section";
 import Container from "../../../common/Container";
 import Header from "../../../common/Header";
-import Section from "../../../common/Section";
-import { useSelector } from "react-redux";
 import { getTaskById } from "../tasksSlice";
 
 const TaskPage = () => {
   const { id } = useParams();
-
   const task = useSelector((state) => getTaskById(state, id));
-
   return (
     <Container>
-      <Header title="Szczegóły zadania" />
-      <Section title={task.content} body={<>nanana</>} />
+      <Header title={"Szczegóły zadania"} />
+      <Section
+        title={task ? task.content : "Nie znaleziono zadania😥"}
+        body={
+          task && (
+            <>
+              <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
+            </>
+          )
+        }
+      />
     </Container>
   );
 };
