cbf2be5d2d2268e0c6a80fa67a5e6f3723d2456b
Urszula Malinowska <ula.malin35@gmail.com>
2023-04-29 20:49:15 +0200

    custom hook useReplaceQueryParameter created and implemented in code

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -2,21 +2,18 @@ import { useLocation, useHistory } from "react-router-dom";
 import Input from "../../Input";
 import { Wrapper } from "./styled";
 import searchQueryParamName from "../searchQueryParamName";
+import { useReplaceQueryParameter } from "../../queryParameters";
 const Search = () => {
   const location = useLocation();
-  const history = useHistory();
   const query = new URLSearchParams(location.search).get(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
 
   const onInputChange = ({ target }) => {
-    const params = new URLSearchParams(location.search);
-    if (!target.value || target.value === "") {
-      params.delete(searchQueryParamName);
-    } else {
-      params.set(searchQueryParamName, target.value);
-    }
-    history.push(`${location.pathname}?${params.toString()}`);
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: target.value.trim() !== "" ? target.value : undefined,
+    });
   };
-
   return (
     <Wrapper>
       <Input
--- src/features/tasks/queryParameters.js
+++ src/features/tasks/queryParameters.js
@@ -1,7 +1,22 @@
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
-
+import { useLocation, useHistory } from "react-router-dom";
 export const useQueryParameter = (param) => {
   const location = useLocation();
   const query = new URLSearchParams(location.search).get(param);
   return query;
+};
+export const useReplaceQueryParameter = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  const params = new URLSearchParams(location.search);
+
+  const replaceQueryParameter = ({ key, value }) => {
+    if (!value) {
+      params.delete(key);
+    } else {
+      params.set(key, value);
+    }
+    return history.push(`${location.pathname}?${params.toString()}`);
+  };
+  return replaceQueryParameter;
 };
