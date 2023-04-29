52c97edc4acf105096c97d80a0682460aba7c74e
Urszula Malinowska <ula.malin35@gmail.com>
2023-04-29 14:45:34 +0200

    filtering tasks in Search component

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,15 +1,29 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import Input from "../../Input";
 import { Wrapper } from "./styled";
+import searchQueryParamName from "../searchQueryParamName";
 const Search = () => {
   const location = useLocation();
+  const history = useHistory();
+  const query = new URLSearchParams(location.search).get(searchQueryParamName);
 
-  const searchParams = new URLSearchParams(location.search);
-  const query = searchParams.get("szukaj");
+  const onInputChange = ({ target }) => {
+    const params = new URLSearchParams(location.search);
+    if (!target.value || target.value === "") {
+      params.delete(searchQueryParamName);
+    } else {
+      params.set(searchQueryParamName, target.value);
+    }
+    history.push(`${location.pathname}?${params.toString()}`);
+  };
 
   return (
     <Wrapper>
-      <Input placeholder="Filtruj zadania" value={query} />
+      <Input
+        placeholder="Filtruj zadania"
+        value={query || ""}
+        onChange={onInputChange}
+      />
     </Wrapper>
   );
 };
